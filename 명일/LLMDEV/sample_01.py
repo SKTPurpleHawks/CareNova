0621094fefc30a1ab12100d5ca11d14e1f5c457a
myeong-il <muyngil@gmail.com>
2025-02-03 23:16:41 +0900

    git push test

--- 명일/LLMDEV/sample_01.py
+++ 명일/LLMDEV/sample_01.py
@@ -12,7 +12,7 @@ openai.api_type = OPENAI_API_TYPE
 openai.api_version = OPENAI_API_VERSION
 openai.azure_endpoint = OPENAI_ENDPOINT
 
-query = input("궁금한걸 물어보세요!: ")
+query = input("궁금한걸 물어보세요!!: ")
 
 response = openai.chat.completions.create(
                 model=MODEL_NAME,
