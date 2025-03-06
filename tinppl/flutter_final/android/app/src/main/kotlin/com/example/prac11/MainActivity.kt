4a17da25ea7281a483135808ca9a621735176736
myeong-il <muyngil@gmail.com>
2025-03-06 14:51:31 +0900

    git project folder modified

--- tinppl/flutter_final/android/app/src/main/kotlin/com/example/prac11/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.prac11
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- 명일/LLMDEV/streamlit.sh
+++
@@ -1,2 +0,0 @@
-pip install streamlit
-python -m streamlit run streamlit_04.py --server.port 8000 --server.address 0.0.0.0
